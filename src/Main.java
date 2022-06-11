66d849fe46c886373f6e432b1e5443ce68db9e1f
mishaf1203 <sduhHDFGH345>
2022-06-11 13:07:43 +0300

    четвёртый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 import jdk.swing.interop.SwingInterOpUtils;
 
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Задача 1");
         byte a = -128;
-        short b = 32 ;
+        short b = 32;
         int c = 64;
-        long d  = 1024L;
+        long d = 1024L;
 
         float x = 8.2f;
         double y = 16.4;
-        System.out.println(a +  ", " + b +", "+ c + ", "+  d + ", "+ x + ", "+ y);
+        System.out.println(a + ", " + b + ", " + c + ", " + d + ", " + x + ", " + y);
 
         char dollar = 36;
         System.out.println(dollar);
@@ -25,20 +24,20 @@ public class Main {
         double boxer1Weight = 78.2;
         double boxer2Weight = 82.7;
         double totalWeight = boxer1Weight + boxer2Weight;
-        System.out.println("Общий вес спортсменов "+ totalWeight + "кг" );
+        System.out.println("Общий вес спортсменов " + totalWeight + "кг");
         double difference = boxer2Weight - boxer1Weight;
-        System.out.println("Разница между весами спорстменов "+ difference);
+        System.out.println("Разница между весами спорстменов " + difference);
 
         System.out.println("Задача 3 ");
         //Задача 3
 
-        short banana = 80;
-        short milk = 105;
-        short iceCream = 100;
-        short eggs = 70;
-        int breakfastWeightGramm = banana * 5 + milk * 2 + iceCream * 2 + eggs * 4;
-        System.out.println("Вес завтрака  "+ breakfastWeightGramm + " грамм");
-        int  breakfastWeightKilo = breakfastWeightGramm / 1000;
+        int banana = 80 * 5;
+        int milk = 105 * 2;
+        int iceCream = 100 * 2;
+        int eggs = 70 * 4;
+        int breakfastWeightGramm = banana + milk + iceCream + eggs;
+        System.out.println("Вес завтрака  " + breakfastWeightGramm + " грамм");
+        double breakfastWeightKilo =  breakfastWeightGramm / 1000.0;
         System.out.println("Вес завтрака  " + breakfastWeightKilo + " килограмм");
 
         System.out.println("Задача 4");
@@ -47,7 +46,7 @@ public class Main {
         int totalWeightLoss = 7000; //1 килограмм равен 1 грамму, следовательно 7000 грамм
         int weightLoss250InDay = totalWeightLoss / 250;
 
-        System.out.println("Понадобится " +weightLoss250InDay + " дней для похудения на 250 грамм в день" );
+        System.out.println("Понадобится " + weightLoss250InDay + " дней для похудения на 250 грамм в день");
 
         int weightLoss500InDay = totalWeightLoss / 500;
         System.out.println("Понадобится " + weightLoss500InDay + " дней для похудения на 500 грамм в день");
@@ -55,7 +54,7 @@ public class Main {
         int weightLossAvg = (weightLoss250InDay + weightLoss500InDay) / 2;
         System.out.println("Понадобится " + weightLossAvg + " в среднем дней для похудения");
 
-        System.out.println("Задача 5  " );
+        System.out.println("Задача 5  ");
         //Задача 5
 
         float MashaSalary = 67760F;
@@ -70,11 +69,11 @@ public class Main {
         double incrDenisSalary = DenisSalary * 1.1;
         double incrKristinaSalary = KristinaSalary * 1.1;
 
-        System.out.println("Зарплата Маши после повышения " +incrMashaSalary  + " рублей");
-        System.out.println("Зарплата Дениса после повышения " +incrDenisSalary  + " рублей" );
-        System.out.println("Зарплата Кристины после повышения " +incrKristinaSalary  + " рублей");
+        System.out.println("Зарплата Маши после повышения " + incrMashaSalary + " рублей");
+        System.out.println("Зарплата Дениса после повышения " + incrDenisSalary + " рублей");
+        System.out.println("Зарплата Кристины после повышения " + incrKristinaSalary + " рублей");
 
-        double incrMashaAnnualSalary = incrMashaSalary  * 12;
+        double incrMashaAnnualSalary = incrMashaSalary * 12;
         double incrDenisAnnualSalary = incrDenisSalary * 12;
         double incrKristinaAnnualSalary = incrKristinaSalary * 12;
 
@@ -83,9 +82,9 @@ public class Main {
         System.out.println(incrDenisAnnualSalary);
         System.out.println(incrKristinaAnnualSalary);
 
-        double differenceMasha =  incrMashaAnnualSalary - MashaAnnualSalary;
-        double differenceDenis =  incrDenisAnnualSalary - DenisAnnualSalary ;
-        double differenceKristina = incrKristinaAnnualSalary - KristinaAnnualSalary ;
+        double differenceMasha = incrMashaAnnualSalary - MashaAnnualSalary;
+        double differenceDenis = incrDenisAnnualSalary - DenisAnnualSalary;
+        double differenceKristina = incrKristinaAnnualSalary - KristinaAnnualSalary;
 
         System.out.println("Разница между годовыми зарплатами до и после повышения Маши, Дениса и Кристины:");
         System.out.println(differenceMasha);
@@ -93,9 +92,7 @@ public class Main {
         System.out.println(differenceKristina);
 
 
-
-        }
-
-
-
     }
+
+
+}
